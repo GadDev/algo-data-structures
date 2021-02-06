9330814b15e8d231af262d32750df3e9d9ea0f41
algadaix <alexandre@qured.com>
2021-02-06 22:35:50 +0000

    algo solution 2 for anagrams

--- anagrams/index.js
+++ anagrams/index.js
@@ -42,5 +42,30 @@ function anagrams(stringA, stringB) {
 	return true;
 }
 
-console.log(anagrams('rail safety', 'fairy tales'));
+// solution with helper function
+function anagramsTwo(stringA, stringB) {
+	const charMapA = buildCharMap(stringA);
+	const charMapB = buildCharMap(stringB);
+
+	if (Object.keys(charMapA).length !== Object.keys(charMapB).length) {
+		return false;
+	}
+
+	for (let char in charMapA) {
+		if (charMapA[char] !== charMapB[char]) {
+			return false;
+		}
+	}
+
+	return true;
+}
+
+function buildCharMap(str) {
+	const charMap = {};
+	for (let char of str.replace(/[^\w]/g, '').toLowercase()) {
+		charMap[char] = charMap[char] + 1 || 1;
+	}
+	return charMap;
+}
+
 module.exports = anagrams;
